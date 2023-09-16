be5210f863a0716c451e68f0ce914c78dd2c59a4
asmsu10420221041506209 <144468278+asmsu10420221041506209@users.noreply.github.com>
2023-09-16 18:36:33 +0530

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,14 @@
+# 2.2'''Implement a class called Player that represents a cricket player. 
+class Player:
+    def play(self):
+        print("The player is playing cricket.")
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting.")
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
+batsman = Batsman()
+bowler = Bowler()
+batsman.play()
+bowler.play()
